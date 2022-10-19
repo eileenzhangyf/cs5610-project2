636186cb76a15cdaddd37a27194dfa2906456a60
eileenzhangyf <zhangbbq@bc.edu>
2022-10-19 15:10:38 -0700

    carrot man added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express()
 const router = express.Router();
 const bodyParser = require('body-parser');
+
+let db;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen(3000,()=>{
@@ -15,8 +18,9 @@ app.use('/stylesheets',express.static(__dirname+'/public/stylesheets'));
 
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://Yifan:8qTRDXVXgD6MwCno@cluster0.ohharax.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri, () => {
+mongoose.connect(uri, (err, database) => {
   console.log("Connected to Mongo DB Successfully!!");
+  db = database;
 })
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/views/index.html'));
@@ -34,7 +38,13 @@ app.use('/buy', buyRouter);
 
 app.use('/',router);
 
+app.get('/buy',(req,res)=>{
+  db.collection('buys').find().toArray((err,result)=>{
+    if (err) return console.log(err);
+    res.send(result);
+  })
 
+});
 
 
 module.exports = app;
