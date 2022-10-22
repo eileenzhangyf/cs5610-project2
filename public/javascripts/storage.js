118327d587793a47cd32c9fe93a63df7333d2608
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-10-22 14:34:05 -0700

    Added eventListeners for all buttons

--- public/javascripts/storage.js
+++ public/javascripts/storage.js
@@ -1,20 +1,18 @@
-// Get things from the edit button
-$(document).ready(function(){
-  $('.editbtn').click(function(){
-      $(this).html($(this).html() == 'edit' ? 'modify' : 'edit');
-  });
+/////////////////////////
+// Listeners
+/////////////////////////
+document.addEventListener("DOMContentLoaded", function() {
+  loadTable();
 });
+
 
 /////////////////////////
 // Functions with Vanilla JS
 /////////////////////////
 function addRow(tableID, content) {
   let tableBodyRef = document.getElementById(tableID);
-  // console.log('table body ref: ' + tableBodyRef)
   let newRow = tableBodyRef.insertRow(-1);
   newRow.innerHTML = content;
-  // console.log('new row: ' + newRow);
-
   tableBodyRef.appendChild(newRow);
 }
 
@@ -25,10 +23,7 @@ function handleResponse(resp) {
 
 function decodeDate(rawDate) {
   let date = new Date(rawDate);
-
-  let ret = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
-  console.log('ret', ret);
-  return ret;
+  return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
 }
 
 function loadTable() {
@@ -37,41 +32,41 @@ function loadTable() {
   fetch('/api/storage/')
     .then(handleResponse)
     .then((data) => {
-      console.log('data: ', data);
+      // console.log('data: ', data);
       
       for (const d in data) {
         console.log(data[d]);
-        let d_date = decodeDate(data[d].purchased);
+        let purchasedDate = data[d].purchased;
+        let d_date = decodeDate(purchasedDate);
+        let daysLast = data[d].daysLast;
+
+        // TODO: Calculate w/ daysLast & pucharsedDate
+        let daysRemained = daysLast; 
+
         let content = `<tr>
               <td data-label="Item">${data[d].name}</td>
               <td data-label="Category">${data[d].category}</td>
               <td data-label="Price">${data[d].price}</td>
               <td data-label="Quantity">${data[d].quantity}</td>
               <td data-label="Purchased Date">${d_date}</td>
-              <td data-label="Remaining Days">${data[d].daysLast}</td>
-              <td data-label="Edit Button"><button class="editbtn">edit</button></td>
+              <td data-label="Remaining Days">${daysRemained}</td>
+              <td data-label="Edit Button"><button class="editBtn">edit</button></td>
               </tr>`
-              // <td data-label="Purchased Date">${purchasedDate}</td>
-              // <td data-label="Remaining Days">${remainedDays}</td>
-        // console.log('content', content);
         addRow(tableID, content);
       }
-      return data;
-    })
-    .catch((e) => {
+    }).then(() => {
+      // Add button listeners
+      let btns = Array.from(document.getElementsByClassName('editBtn'));
+
+      btns.forEach(btn => {
+        btn.addEventListener('click', function handleClick(event) {
+          // console.log('Edit button clicked', event);
+          if (btn.innerText == "edit") btn.innerText = "modify";
+          else btn.innerText = "edit";
+        });
+      });
+    }).catch((e) => {
       console.log(`Failed: loading all storage items. ${e}`);
     });
-
-  
-    
-
-  
-  // for (let i = 0; i < 3; i++) {
-  //   addRow(tableID, content);
-  // }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  loadTable();
-  // fetchData();
-});
