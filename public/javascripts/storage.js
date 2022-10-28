e7daeda9eb49abe39b922b0f0c0e50f2516f097e
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-10-28 16:36:10 -0700

    Edited storage feature and cleaned up

--- public/javascripts/storage.js
+++ public/javascripts/storage.js
@@ -50,19 +50,35 @@ function loadTable() {
               <td data-label="Quantity">${data[d].quantity}</td>
               <td data-label="Purchased Date">${d_date}</td>
               <td data-label="Remaining Days">${daysRemained}</td>
-              <td data-label="Edit Button"><button class="editBtn">edit</button></td>
+              <td data-label="Actions">
+                <button class="actionBtn editBtn">edit</button>
+                <button class="actionBtn deleteBtn">delete</button>
+                </td>
               </tr>`
         addRow(tableID, content);
       }
     }).then(() => {
       // Add button listeners
-      let btns = Array.from(document.getElementsByClassName('editBtn'));
+      let editBtns = Array.from(document.getElementsByClassName('editBtn'));
 
-      btns.forEach(btn => {
+      editBtns.forEach(btn => {
         btn.addEventListener('click', function handleClick(event) {
           // console.log('Edit button clicked', event);
-          if (btn.innerText == "edit") btn.innerText = "modify";
-          else btn.innerText = "edit";
+          console.log('button: ', btn);
+          let row = btn.parentElement.parentElement;
+          console.log("cell row: ", row);
+          console.log("1:  ", row.children[1].innerHTML);
+
+          if (btn.innerText == "edit") {
+            row2input(row);
+
+
+            btn.innerText = "ok";
+          } else {
+            
+
+            btn.innerText = "edit";
+          }
         });
       });
     }).catch((e) => {
@@ -70,3 +86,52 @@ function loadTable() {
     });
 }
 
+function row2input(row) {
+  let vals = [];
+  for (let i = 0; i <= 5; i++) {
+    vals[i] = row.children[i].innerHTML;
+  }
+  console.log("vals: ", vals);
+  // let item = row.children[0].innerHTML;
+  // let category = row.children[1].innerHTML;
+  // let price = row.children[2].innerHTML;
+  // let quantity = row.children[3].innerHTML;
+  // let purchasedDate = row.children[4].innerHTML;
+  // let daysLast = row.children[5].innerHTML;
+
+  let newRow = 
+  `<tr>
+  <td><input type="text" id="item" name="item"/></td>
+  <td><input type="text" id="category" name="category"/></td>
+  <td><input type="number" id="price" name="price"/></td>
+  <td><input type="number" id="quantity" name="quantity"/></td>
+  <td><input type="date" id="purchased-date" name="purchased-date"/></td>
+  <td><input type="number" id="days-last" name="days-last"/></td>
+</tr>`;
+  
+  const nRow = document.createElement("tr");
+  for (let i = 0; i <= 5; i++) {
+    const cell = document.createElement("td");
+    let input = document.createElement("input");
+    input.type = inputTypes[i];
+    input.id = "id";
+    input.name = "name";
+    input.value = "555";
+    cell.append(input);
+    nRow.append(cell);
+  }
+  
+  console.log(typeof (row) + " " + typeof (nRow));
+  console.log("new cell row: ", row);
+  
+  console.log("new cell nRow: ", nRow);
+}
+
+const inputTypes = {
+  0: ["text", "item", "item"],
+  1: ["text", "category", "category"],
+  2: ["number","price","price"],
+  3: ["number","quantity","quantity"],
+  4: ["date","purchased-date","purchased-date"],
+  5: ["number","days-last","days-last"]
+}
