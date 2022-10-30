a3c6e93bf0010c030732803af569e8d1236e8983
eileenzhangyf <zhangbbq@bc.edu>
2022-10-30 16:59:48 -0700

    port updated

--- db/mongoUtil.js
+++ db/mongoUtil.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = process.env.URI_SHANE || "mongodb://localhost:27017/foodkeeper";
+const uri = process.env.PORT || "mongodb://localhost:27017/foodkeeper";
 let _db;
 
 module.exports = {
