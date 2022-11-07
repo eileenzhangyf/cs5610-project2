13fc7ad9740103e961fa22446f6c0299887d8c33
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2022-11-07 10:25:30 -0800

    Removed unused and unrelated files

--- models/buyModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-// create an schema
-const buySchema = new mongoose.Schema({  
-            name: {type:String},
-            curr_date: {type:Date}
-        
-    });
-module.exports = mongoose.model('Buy',buySchema);
